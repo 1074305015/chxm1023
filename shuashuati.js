8c3d3a187e891dbe945695290eb87fffd5d39669
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2023-08-19 23:19:49 +0800

    Update shuashuati.js

--- shuashuati.js
+++ shuashuati.js
@@ -19,26 +19,41 @@ hostname = api.shuashuati.com
 
 var body = $response.body;
 
+function deepDelete(chxm1024, propToDelete) {
+  for (let prop in chxm1024) {
+    if (prop === propToDelete) {
+      delete chxm1024[prop];
+    } else if (typeof chxm1024[prop] === "object") {
+      deepDelete(chxm1024[prop], propToDelete);
+    }
+  }
+}
+var chxm1023 = JSON.parse(body);
+deepDelete(chxm1023, ['adInfo','defaultAd']);
+body = JSON.stringify(chxm1023);
+
 body = body.replace(/\"state":\d+/g, '\"state":2');
 
 body = body.replace(/\"vipToTime":\w+/g, '\"vipToTime":"2099-09-09 09:09:09"');
 
 body = body.replace(/\"vipToTimeStr":\w+/g, '\"vipToTimeStr":"2099-09-09 09:09:09"');
 
-body = body.replace(/\"ad":\d+/g, '\"ad":0');
+body = body.replace(/\"first":\d+/g, '\"first":1');
 
-body = body.replace(/\"adInfo"/g, '\"chxm1023"');
+body = body.replace(/\"userDou":\d+/g, '\"userDou":1');
+
+body = body.replace(/\"balance":\w+/g, '\"balance":1');
+
+body = body.replace(/\"adType":"(.*?)"/g, '\"adType":null');
+
+body = body.replace(/\"pay":\d+/g, '\"pay":0');
+
+body = body.replace(/\"pay":\w+/g, '\"pay":false');
 
 body = body.replace(/\"free":\d+/g, '\"free":1');
 
-body = body.replace(/\"iosPay":\d+/g, '\"iosPay":2');
+body = body.replace(/\"word_vip":\d+/g, '\"word_vip":1');
 
-body = body.replace(/\"word_vip":\d+/g, '\"word_vip":2');
-
-body = body.replace(/\"home_dou_vip":\d+/g, '\"home_dou_vip":2');
-
-body = body.replace(/\"math_vip":\d+/g, '\"math_vip":2');
-
-body = body.replace(/\"audit":\d+/g, '\"audit":1');
+body = body.replace(/\"from":\d+/g, '\"from":0');
 
 $done({body});
