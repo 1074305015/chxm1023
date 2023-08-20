51ceff000d0c0a89e944ea5e261e3c5006cba15c
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2023-08-20 15:38:53 +0800

    Create zjsg.js

---
+++ zjsg.js
@@ -0,0 +1,24 @@
+/*************************************
+
+项目名称：指尖时光
+下载地址：https://too.st/5Dh
+脚本作者：chxm1023
+电报频道：https://t.me/chxm1023
+使用声明：⚠️仅供参考，🈲转载与售卖！
+
+**************************************
+
+[rewrite_local]
+^https?:\/\/integral2\.dasyibalang\.com\/v2\/User url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/zjsg.js
+
+[mitm]
+hostname = integral2.dasyibalang.com
+
+*************************************/
+
+
+var chxm1023 = JSON.parse($response.body);
+
+chxm1023.data = { ...chxm1023.data, VIP: 3, MembersBeginDateMs: true };
+
+$done({body : JSON.stringify(chxm1023)});
