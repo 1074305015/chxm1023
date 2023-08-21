0d02e00c036ef496740ed9f0416e358bbccd3ff5
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2023-08-21 15:24:40 +0800

    更新 xinzang.js

--- xinzang.js
+++ xinzang.js
@@ -17,17 +17,21 @@ hostname = api.995120.cn
 *************************************/
 
 
-var chxm1023 = JSON.parse($response.body);
+var body = $response.body;
+var newYearCard = '\"yearCard\":{\"goodNo\" : "dc3c24a224cc41fe804108c856ebfd9c",\"status\" : 1,\"content\" : \"叮当猫の分享频道\" ,\"endTime\" : \"2099-09-09到期\",\"type\" : 3,\"serviceBalanceId\" : 2501498,\"goodName\" : "心脏+包年"}';
+var newexperienceCard = '\"experienceCard\":{\"status\" : 1,\"content\" : \"体验卡剩余3次\",\"balanceNum\" : 99999999,\"endTime\" : \"2099-09-09到期\",\"balanceName\" : \"极速心电图（ECG）分析服务\",\"type\" : 1,\"serviceBalanceId\" : 9999999}';
 
-chxm1023.body.data.endTime = "2099-09-09到期";
-chxm1023.body.data.yearCard = {
-        "status" : 1,
-        "content" : "叮当猫の分享频道",
-        "balanceNum" : 99999999,
-        "endTime" : "2099-09-09到期",
-        "balanceName" : "极速心电图（ECG）分析服务",
-        "type" : 1,
-        "serviceBalanceId" : 2501498
-};
+body = body.replace(/\"endTime":".*?"/g, '\"endTime":"2099-09-09到期"');
 
-$done({body : JSON.stringify(chxm1023)});
+body = body.replace(/\"hwMembershipDate":".*?"/g, '\"hwMembershipDate":"2099-09-09到期"');
+
+body = body.replace(/\"terminalType":"\d+"/g, '\"terminalType":"0"');
+
+body = body.replace(/\"result":\w+/g, '\"result":true');
+
+body = body.replace(/\"yearCard\":\{.*?\}/g, newYearCard);
+body = body.replace(/\"experienceCard\":\{.*?\}/g, newexperienceCard);
+
+body = body.replace(/\"sex":\d+/g, '\"sex":1');
+
+$done({body});
