46fea1f1909f2a32072988cb1c4f7d248043d27a
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2023-08-28 10:26:58 +0800

    更新 chaoxi.js

--- chaoxi.js
+++ chaoxi.js
@@ -19,7 +19,7 @@ hostname = tide-api.moreless.io
 
 var chxm1023 = JSON.parse($response.body);
 
-chxm1023.vip = {
+chxm1023["vip"] = {
     "expired_at" : 4092599349,
     "is_valid" : true,
     "created_at" : 1684249596,
@@ -27,6 +27,6 @@ chxm1023.vip = {
     "is_lifetime_member" : true,
     "updated_at" : 1684249596,
     "premium_type" : 2
-  };
+};
 
 $done({body : JSON.stringify(chxm1023)});
