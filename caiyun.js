614f19260f8ced7332fab8ee19beec6ae964bf8a
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2023-09-11 18:32:55 +0800

    更新 caiyun.js

--- caiyun.js
+++ caiyun.js
@@ -20,61 +20,63 @@ hostname = *.cyapi.cn, adx.sogaha.cn
 
 
 var chxm1023 = JSON.parse($response.body);
-const vip = '/vip_info';
-const adhf = '/activity';
-const user = '(user|visitors)';
+const urls = ['/vip_info', '/activity', 'user'];
 
-if ($request.url.indexOf(vip) != -1){
-    chxm1023.vip = {
-        "expires_time" : "4092599349",
-        "is_auto_renewal" : true
-      };
-    chxm1023.svip = {
-        "expires_time" : "4092599349",
-        "is_auto_renewal" : true
-      };
-}
-
-if ($request.url.indexOf(adhf) != -1){
-    chxm1023.activities = [];
-}
-
-if ($request.url.indexOf('user') != -1){
-    chxm1023.result.ranking_above = 99;
-    chxm1023.result.is_vip = true;
-    chxm1023.result.vip_expired_at = 4092599349;
-    chxm1023.result.svip_given = 9999;
-    chxm1023.result.is_xy_vip = true;
-    chxm1023.result.xy_svip_expire = 4092599349; 
-    chxm1023.result.wt.vip = {
-        "auto_renewal_type" : "",
-        "expired_at" : 0,
-        "enabled" : true,
-        "svip_apple_expired_at" : 4092599349,
-        "is_auto_renewal" : true,
-        "svip_expired_at" : 4092599349,
-        "svip_auto_renewal_type" : ""
-      };
-    chxm1023.result.wt.svip_given = 9999;
-    chxm1023.result.wt.ranking_above = 99;
-    chxm1023.result.is_phone_verified = true;
-    chxm1023.result.name = "叮当猫の分享频道";
-    chxm1023.result.avatar = "https://thirdwx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTI6OLWyDbM42w944dUdfzrPEQv6uaD3AEsVcicibYibxG9PBBSaibwqRJzOk2US1d8N4hC9nL1a5rXu3g/132";
-    chxm1023.result.phone_num = "13145200000";
-    chxm1023.result.vip_take_effect = 1;
-    chxm1023.result.is_primary = true;
-    chxm1023.result.xy_vip_expire = 4092599349;
-    chxm1023.result.svip_expired_at = 4092599349;
-    chxm1023.result.svip_take_effect = 1;
-    chxm1023.result.vip_type = "s";
-    chxm1023.result.token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ2ZXJzaW9uIjoxLCJ1c2VyX2lkIjoiNWY1YmZjNTdkMmM2ODkwMDE0ZTI2YmI4Iiwic3ZpcF9leHBpcmVkX2F0IjoxNzA1MzMxMTY2LjQxNjc3MSwidmlwX2V4cGlyZWRfYXQiOjB9.h_Cem89QarTXxVX9Z_Wt-Mak6ZHAjAJqgv3hEY6wpps";
-    chxm1023.result.bound_status.qq.username = "https://t.me/chxm1023";
-    chxm1023.result.bound_status.weixin.username = "https://t.me/chxm1023";
-    chxm1023.result.bound_status.caiyun.username = "https://t.me/chxm1023";
-    chxm1023.result.bound_status.twitter.username = "https://t.me/chxm1023";
-    chxm1023.result.bound_status.facebook.username = "https://t.me/chxm1023";
-    chxm1023.result.bound_status.apple.username = "https://t.me/chxm1023";
-    chxm1023.result.bound_status.weibo.username = "https://t.me/chxm1023";
-}
+urls.forEach(url => {
+  if ($request.url.includes(url)){
+    switch(url) {
+      case '/vip_info':
+        chxm1023 = {
+          ...chxm1023, 
+          vip: {
+            "expires_time" : "4092599349",
+            "is_auto_renewal" : true
+          },
+          svip: {
+            "expires_time" : "4092599349",
+            "is_auto_renewal" : true
+          }
+        }
+        break;
+      case '/activity':
+        chxm1023.activities = [];
+        break;
+      case 'user':
+        chxm1023.result = {
+          ...chxm1023.result,
+          ranking_above: 99,
+          is_vip: true,
+          vip_expired_at: 4092599349,
+          svip_given: 9999,
+          is_xy_vip: true,
+          xy_svip_expire: 4092599349,
+          wt: {
+            ...chxm1023.result.wt,
+            vip: {
+              "auto_renewal_type" : "",
+              "expired_at" : 0,
+              "enabled" : true,
+              "svip_apple_expired_at" : 4092599349,
+              "is_auto_renewal" : true,
+              "svip_expired_at" : 4092599349,
+              "svip_auto_renewal_type" : ""
+            },
+            svip_given: 9999,
+            ranking_above: 99,
+          },
+          is_phone_verified: true,
+          phone_num: "13145200000",
+          vip_take_effect: 1,
+          is_primary: true,
+          xy_vip_expire: 4092599349,
+          svip_expired_at: 4092599349,
+          svip_take_effect: 1,
+          vip_type: "s",
+          token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ2ZXJzaW9uIjoxLCJ1c2VyX2lkIjoiNWY1YmZjNTdkMmM2ODkwMDE0ZTI2YmI4Iiwic3ZpcF9leHBpcmVkX2F0IjoxNzA1MzMxMTY2LjQxNjc3MSwidmlwX2V4cGlyZWRfYXQiOjB9.h_Cem89QarTXxVX9Z_Wt-Mak6ZHAjAJqgv3hEY6wpps"
+        }
+        break;
+    }
+  }
+})
 
 $done({ body: JSON.stringify(chxm1023)});
