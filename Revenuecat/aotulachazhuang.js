9a129075c41ad6f467266c18897fbf8b7871730f
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2023-09-24 02:14:41 +0800

    Create aotulachazhuang.js

---
+++ Revenuecat/aotulachazhuang.js
@@ -0,0 +1,37 @@
+/*************************************
+
+项目名称：凹凸啦查妆
+下载地址：https://t.cn/A6OQyLvq
+脚本作者：chxm1023
+电报频道：https://t.me/chxm1023
+使用声明：⚠️仅供参考，🈲转载与售卖！
+
+**************************************
+
+[rewrite_local]
+^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/Revenuecat/aotulachazhuang.js
+^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/chxm1023/Rewrite/main/Revenuecat/aotulachazhuang.js
+
+[mitm]
+hostname = api.revenuecat.com
+
+*************************************/
+
+
+const chxm1023 = {};
+const chxm1024 = JSON.parse(typeof $response != "undefined" && $response.body || null);
+const name = "Pro access";
+const appid = "com.smartitfarmer.MakeUpAssistant.UNLIMITED";
+
+if (typeof $response == "undefined") {
+  delete $request.headers["x-revenuecat-etag"];
+  delete $request.headers["X-RevenueCat-ETag"];
+  chxm1023.headers = $request.headers;
+} else if (chxm1024 && chxm1024.subscriber) {
+  const data = {  "product_identifier": (appid),  	"expires_date": "2099-09-09T09:09:09Z",	"purchase_date": "2022-09-09T09:09:09Z"	};
+  chxm1024.subscriber.entitlements[(name)] = (data);
+  chxm1024.subscriber.subscriptions[(appid)] = {  ...data,	"Author": "chxm1023",	"Telegram": "https://t.me/chxm1023",	"warning": "仅供学习，禁止转载或售卖",	"original_purchase_date": "2022-09-09T09:09:09Z",	"store": "app_store",	"ownership_type": "PURCHASED"};
+  chxm1023.body = JSON.stringify(chxm1024);
+}
+
+$done(chxm1023);
