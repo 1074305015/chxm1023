b00af468a8039c7d18fbeadc961d0b35f4c3b70a
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2023-09-28 19:49:18 +0800

    Create 20geapp.js

---
+++ 20geapp.js
@@ -0,0 +1,30 @@
+/*************************************
+
+项目名称：20个APP解锁全家桶
+下载地址：https://t.cn/A6Ouq9uD
+脚本作者：chxm1023
+电报频道：https://t.me/chxm1023
+使用声明：⚠️仅供参考，🈲转载与售卖！
+
+**************************************
+
+[rewrite_local]
+^https?:\/\/(appss|standard)\.(rhinox.*|linhongshi)\.com\/.+\/account\/getAccountInfo url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/20geapp.js
+
+[mitm]
+hostname = *.rhinox*.com, appss.linhongshi.com
+
+*************************************/
+
+
+var chxm1023 = JSON.parse($response.body);
+const ua = $request.headers['User-Agent'] || $request.headers['user-agent'];
+
+if (ua.indexOf('bnchangtu') != -1) {
+  chxm1023.result["type"] = "FOREVER";
+} else { chxm1023.result["type"] = "VIP"; }
+chxm1023.result["vipGroupInfos"] = [      {        "groupType" : "TYPE_ONE",        "vipType" : "VIP",        "autoPay" : "YES"      }    ];
+chxm1023.result["vipExpireTime"] = "2099-09-09 09:09:09";
+chxm1023.result["vipExpireDays"] = 999999;
+
+$done({body : JSON.stringify(chxm1023)});
