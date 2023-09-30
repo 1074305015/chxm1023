385107a1f89f487737eb53a41554ae0c51dbaab6
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2023-09-30 11:28:45 +0800

    Create xiaoxiong.js

---
+++ Revenuecat/xiaoxiong.js
@@ -0,0 +1,52 @@
+/*************************************
+
+项目名称：小熊记账/轻断食
+下载地址：https://t.cn/A6O1XBAQ
+下载地址：https://t.cn/A6O1XnIt
+脚本作者：chxm1023
+电报频道：https://t.me/chxm1023
+使用声明：⚠️仅供参考，🈲转载与售卖！
+
+**************************************
+
+[rewrite_local]
+^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/Revenuecat/xiaoxiong.js
+^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/chxm1023/Rewrite/main/Revenuecat/xiaoxiong.js
+
+[mitm]
+hostname = api.revenuecat.com
+
+*************************************/
+
+
+const chxm1023 = {};
+const chxm1024 = JSON.parse(typeof $response != "undefined" && $response.body || null);
+//小熊记账
+if (ua.indexOf('money_manager') != -1) {
+  name = "premium";
+  appid = "com.happy.money.forever";
+}
+//小熊轻断食
+if (ua.indexOf('fastdiet') != -1) {
+  name = "premium";
+  appid = "com.happy.fastdiet.forever";
+}
+
+if (typeof $response == "undefined") {
+  delete $request.headers["x-revenuecat-etag"];
+  delete $request.headers["X-RevenueCat-ETag"];
+  chxm1023.headers = $request.headers;
+} else if (chxm1024 && chxm1024.subscriber) {
+  chxm1024.subscriber.subscriptions = chxm1024.subscriber.subscriptions || {};
+  chxm1024.subscriber.entitlements = chxm1024.subscriber.entitlements || {};
+  const data = {
+	"product_identifier": (appid),
+	"expires_date": "2099-09-09T09:09:09Z",
+	"purchase_date": "2022-09-09T09:09:09Z"
+	};
+  chxm1024.subscriber.entitlements[(name)] = (data);
+  chxm1024.subscriber.subscriptions[(appid)] = {  ...data,	"Author": "chxm1023",	"Telegram": "https://t.me/chxm1023",	"warning": "仅供学习，禁止转载或售卖",	"original_purchase_date": "2022-09-09T09:09:09Z",	"store": "app_store",	"ownership_type": "PURCHASED"};
+  chxm1023.body = JSON.stringify(chxm1024);
+}
+
+$done(chxm1023);
