89564d374b6e68d62ae8334b1bb40c51d37f7267
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2023-10-02 09:33:59 +0800

    Create bgds.js

---
+++ iTunes/bgds.js
@@ -0,0 +1,99 @@
+/*************************************
+
+项目名称：表格大师
+下载地址：https://t.cn/A6OrDmxb
+脚本作者：chxm1023
+电报频道：https://t.me/chxm1023
+使用声明：⚠️仅供参考，🈲转载与售卖！
+
+**************************************
+
+[rewrite_local]
+^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/iTunes/bgds.js
+
+[mitm]
+hostname = buy.itunes.apple.com
+
+*************************************/
+
+
+var chxm1023 = JSON.parse($response.body);
+
+chxm1023 = {
+  "status" : 0,
+  "receipt" : {
+    "receipt_type" : "Production",
+    "app_item_id" : 1518721142,
+    "receipt_creation_date" : "2023-09-09 16:06:26 Etc/GMT",
+    "bundle_id" : "com.bestmusicvideo.formmaster",
+    "original_purchase_date" : "2023-09-09 16:00:00 Etc/GMT",
+    "in_app" : [
+      {
+        "quantity" : "1",
+        "purchase_date_ms" : "1694250549000",
+        "expires_date" : "2099-09-09 09:09:09 Etc/GMT",
+        "expires_date_pst" : "2099-09-09 06:06:06 America/Los_Angeles",
+        "is_in_intro_offer_period" : "false",
+        "transaction_id" : "490001314520000",
+        "is_trial_period" : "false",
+        "original_transaction_id" : "490001314520000",
+        "purchase_date" : "2023-09-09 09:09:09 Etc/GMT",
+        "product_id" : "com.form.1yearvip",
+        "original_purchase_date_pst" : "2023-09-09 02:09:10 America/Los_Angeles",
+        "in_app_ownership_type" : "PURCHASED",
+        "original_purchase_date_ms" : "1694250550000",
+        "web_order_line_item_id" : "490000123456789",
+        "expires_date_ms" : "4092599349000",
+        "purchase_date_pst" : "2023-09-09 02:09:09 America/Los_Angeles",
+        "original_purchase_date" : "2023-09-09 09:09:10 Etc/GMT"
+      }
+    ],
+    "adam_id" : 1518721142,
+    "receipt_creation_date_pst" : "2023-09-09 06:06:26 America/Los_Angeles",
+    "request_date" : "2023-09-09 16:06:27 Etc/GMT",
+    "request_date_pst" : "2023-09-09 06:06:27 America/Los_Angeles",
+    "version_external_identifier" : 860061577,
+    "request_date_ms" : "1694273635000",
+    "original_purchase_date_pst" : "2023-09-09 06:00:00 America/Los_Angeles",
+    "application_version" : "10",
+    "original_purchase_date_ms" : "1694273430000",
+    "receipt_creation_date_ms" : "1694273634000",
+    "original_application_version" : "10",
+    "download_id" : 502820827146367940
+  },
+  "latest_receipt_info" : [
+    {
+      "quantity" : "1",
+      "purchase_date_ms" : "1694250549000",
+      "expires_date" : "2099-09-09 09:09:09 Etc/GMT",
+      "expires_date_pst" : "2099-09-09 06:06:06 America/Los_Angeles",
+      "is_in_intro_offer_period" : "false",
+      "transaction_id" : "490001314520000",
+      "is_trial_period" : "false",
+      "original_transaction_id" : "490001314520000",
+      "purchase_date" : "2023-09-09 09:09:09 Etc/GMT",
+      "product_id" : "com.form.1yearvip",
+      "original_purchase_date_pst" : "2023-09-09 02:09:10 America/Los_Angeles",
+      "in_app_ownership_type" : "PURCHASED",
+      "original_purchase_date_ms" : "1694250550000",
+      "web_order_line_item_id" : "490000123456789",
+      "expires_date_ms" : "4092599349000",
+      "purchase_date_pst" : "2023-09-09 02:09:09 America/Los_Angeles",
+      "original_purchase_date" : "2023-09-09 09:09:10 Etc/GMT"
+    }
+  ],
+  "latest_receipt" : "chxm1023",
+  "environment" : "Production",
+  "pending_renewal_info" : [
+    {
+      "product_id" : "com.form.1yearvip",
+      "original_transaction_id" : "490001314520000",
+      "auto_renew_product_id" : "com.form.1yearvip",
+      "auto_renew_status" : "1"
+    }
+  ],
+  "warning" : "仅供学习，禁止转载或售卖",
+  "Telegram" : "https://t.me/chxm1023"
+};
+
+$done({body: JSON.stringify(chxm1023)});
