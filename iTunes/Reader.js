ba0ae32897564eafee8b97c6d6e1b9ca9dc56d72
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2023-10-03 14:20:54 +0800

    Create Reader.js

---
+++ iTunes/Reader.js
@@ -0,0 +1,89 @@
+/*************************************
+
+项目名称：爱阅读-TXT阅读器
+下载地址：https://t.cn/A6OgaAth
+脚本作者：chxm1023
+电报频道：https://t.me/chxm1023
+使用声明：⚠️仅供参考，🈲转载与售卖！
+
+**************************************
+
+[rewrite_local]
+^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/iTunes/Reader.js
+
+[mitm]
+hostname = buy.itunes.apple.com
+
+*************************************/
+
+
+var chxm1023 = JSON.parse($response.body);
+
+chxm1023 = {
+  "status" : 0,
+  "receipt" : {
+    "receipt_type" : "Production",
+    "app_item_id" : 1671968704,
+    "receipt_creation_date" : "2023-09-09 16:06:26 Etc/GMT",
+    "bundle_id" : "com.xiaoqi.reader",
+    "original_purchase_date" : "2023-09-09 16:00:00 Etc/GMT",
+    "in_app" : [
+      {
+        "quantity" : "1",
+        "purchase_date_ms" : "1694250549000",
+        "transaction_id" : "490001314520000",
+        "is_trial_period" : "false",
+        "original_transaction_id" : "490001314520000",
+        "purchase_date" : "2023-09-09 09:09:09 Etc/GMT",
+        "product_id" : "com.xiaoqi.reader.forever",
+        "original_purchase_date_pst" : "2023-09-09 02:09:10 America/Los_Angeles",
+        "in_app_ownership_type" : "PURCHASED",
+        "original_purchase_date_ms" : "1694250550000",
+        "purchase_date_pst" : "2023-09-09 02:09:09 America/Los_Angeles",
+        "original_purchase_date" : "2023-09-09 09:09:10 Etc/GMT"
+      }
+    ],
+    "adam_id" : 1671968704,
+    "receipt_creation_date_pst" : "2023-09-09 06:06:26 America/Los_Angeles",
+    "request_date" : "2023-09-09 16:06:27 Etc/GMT",
+    "request_date_pst" : "2023-09-09 06:06:27 America/Los_Angeles",
+    "version_external_identifier" : 860155228,
+    "request_date_ms" : "1694273635000",
+    "original_purchase_date_pst" : "2023-09-09 06:00:00 America/Los_Angeles",
+    "application_version" : "2",
+    "original_purchase_date_ms" : "1694273430000",
+    "receipt_creation_date_ms" : "1694273634000",
+    "original_application_version" : "2",
+    "download_id" : 502834132663514240
+  },
+  "latest_receipt_info" : [
+    {
+      "quantity" : "1",
+      "purchase_date_ms" : "1694250549000",
+      "transaction_id" : "490001314520000",
+      "is_trial_period" : "false",
+      "original_transaction_id" : "490001314520000",
+      "purchase_date" : "2023-09-09 09:09:09 Etc/GMT",
+      "product_id" : "com.xiaoqi.reader.forever",
+      "original_purchase_date_pst" : "2023-09-09 02:09:10 America/Los_Angeles",
+      "in_app_ownership_type" : "PURCHASED",
+      "original_purchase_date_ms" : "1694250550000",
+      "purchase_date_pst" : "2023-09-09 02:09:09 America/Los_Angeles",
+      "original_purchase_date" : "2023-09-09 09:09:10 Etc/GMT"
+    }
+  ],
+  "latest_receipt" : "chxm1023",
+  "environment" : "Production",
+  "pending_renewal_info" : [
+    {
+      "product_id" : "com.xiaoqi.reader.forever",
+      "original_transaction_id" : "490001314520000",
+      "auto_renew_product_id" : "com.xiaoqi.reader.forever",
+      "auto_renew_status" : "1"
+    }
+  ],
+  "warning" : "仅供学习，禁止转载或售卖",
+  "Telegram" : "https://t.me/chxm1023"
+};
+
+$done({body: JSON.stringify(chxm1023)});
