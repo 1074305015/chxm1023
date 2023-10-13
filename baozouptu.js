b759dd685f383c107f0e896fec5c698003e51f76
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2023-10-13 19:44:10 +0800

    Create baozouptu.js

---
+++ baozouptu.js
@@ -0,0 +1,25 @@
+/*************************************
+
+项目名称：暴走P图
+下载地址：https://t.cn/A6WwGEm7
+脚本作者：chxm1023
+电报频道：https://t.me/chxm1023
+使用声明：⚠️仅供参考，🈲转载与售卖！
+
+**************************************
+
+[rewrite_local]
+https?:\/\/api\.intelimeditor\.com\/user\/loginByThirdPlatformApp url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/baozouptu.js
+
+[mitm]
+hostname = api.intelimeditor.com
+
+*************************************/
+
+
+var chxm1023 = JSON.parse($response.body);
+
+chxm1023.data.lookVipCount = 3;
+chxm1023.data.vipExpireTime = "4092599349000";
+
+$done({body : JSON.stringify(chxm1023)});
