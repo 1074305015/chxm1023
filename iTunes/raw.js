c6444373ca49d02610f1e8e05eb736ca23db2ef3
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2023-10-14 09:37:43 +0800

    更新 raw.js

--- iTunes/raw.js
+++ iTunes/raw.js
@@ -31,14 +31,19 @@ chxm1023 = {
       {
         "quantity" : "1",
         "purchase_date_ms" : "1694250549000",
+        "expires_date" : "2099-09-09 09:09:09 Etc/GMT",
+        "expires_date_pst" : "2099-09-09 06:06:06 America/Los_Angeles",
+        "is_in_intro_offer_period" : "false",
         "transaction_id" : "490001314520000",
         "is_trial_period" : "false",
         "original_transaction_id" : "490001314520000",
         "purchase_date" : "2023-09-09 09:09:09 Etc/GMT",
-        "product_id" : "com.dynamicappdesign.rawplus.lifetimelysubscription",
+        "product_id" : "com.dynamicappdesign.rawplus.yearlysubscription",
         "original_purchase_date_pst" : "2023-09-09 02:09:10 America/Los_Angeles",
         "in_app_ownership_type" : "PURCHASED",
         "original_purchase_date_ms" : "1694250550000",
+        "web_order_line_item_id" : "490000123456789",
+        "expires_date_ms" : "4092599349000",
         "purchase_date_pst" : "2023-09-09 02:09:09 America/Los_Angeles",
         "original_purchase_date" : "2023-09-09 09:09:10 Etc/GMT"
       }
@@ -53,21 +58,26 @@ chxm1023 = {
     "application_version" : "78",
     "original_purchase_date_ms" : "1694273430000",
     "receipt_creation_date_ms" : "1694273634000",
-    "original_application_version" : "78",
-    "download_id" : 502863400637247360
+    "original_application_version" : "27",
+    "download_id" : 80084171907107
   },
   "latest_receipt_info" : [
     {
       "quantity" : "1",
       "purchase_date_ms" : "1694250549000",
+      "expires_date" : "2099-09-09 09:09:09 Etc/GMT",
+      "expires_date_pst" : "2099-09-09 06:06:06 America/Los_Angeles",
+      "is_in_intro_offer_period" : "false",
       "transaction_id" : "490001314520000",
       "is_trial_period" : "false",
       "original_transaction_id" : "490001314520000",
       "purchase_date" : "2023-09-09 09:09:09 Etc/GMT",
-      "product_id" : "com.dynamicappdesign.rawplus.lifetimelysubscription",
+      "product_id" : "com.dynamicappdesign.rawplus.yearlysubscription",
       "original_purchase_date_pst" : "2023-09-09 02:09:10 America/Los_Angeles",
       "in_app_ownership_type" : "PURCHASED",
       "original_purchase_date_ms" : "1694250550000",
+      "web_order_line_item_id" : "490000123456789",
+      "expires_date_ms" : "4092599349000",
       "purchase_date_pst" : "2023-09-09 02:09:09 America/Los_Angeles",
       "original_purchase_date" : "2023-09-09 09:09:10 Etc/GMT"
     }
@@ -76,9 +86,9 @@ chxm1023 = {
   "environment" : "Production",
   "pending_renewal_info" : [
     {
-      "product_id" : "com.dynamicappdesign.rawplus.lifetimelysubscription",
+      "product_id" : "com.dynamicappdesign.rawplus.yearlysubscription",
       "original_transaction_id" : "490001314520000",
-      "auto_renew_product_id" : "com.dynamicappdesign.rawplus.lifetimelysubscription",
+      "auto_renew_product_id" : "com.dynamicappdesign.rawplus.yearlysubscription",
       "auto_renew_status" : "1"
     }
   ],
